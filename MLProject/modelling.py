00de56b3daa44a831bcb6623659363f3271960e2
DiosiPutriArlita <147496815+DiosiPutriArlita@users.noreply.github.com>
2026-06-04 17:31:50 +0700

    Add files via upload

---
+++ MLProject/modelling.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+
+# Load dataset preprocessing
+df = pd.read_csv("travel_insurance_preprocessing.csv")
+
+X = df.drop("TravelInsurance", axis=1)
+y = df["TravelInsurance"]
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+# MLflow Tracking lokal
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("Travel Insurance Prediction")
+
+# Autolog untuk memenuhi Basic Kriteria 2
+mlflow.sklearn.autolog()
+
+with mlflow.start_run():
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    print("Model training selesai.")
+    print("Accuracy :", accuracy)
+    print("Precision:", precision)
+    print("Recall   :", recall)
+    print("F1 Score :", f1)
